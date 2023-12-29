e93a7b98588c89481847c93f0401c54a5b95abff
iamseung <seesungseok@naver.com>
2023-12-29 22:13:40 +0900

    QueryDSL 의존성 추가

--- src/test/java/hello/core/controller/DataRestTest.java
+++ src/test/java/hello/core/controller/DataRestTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -25,8 +26,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
     요청 및 전송, 응답기능을 제공해주는 유틸리티 클래스
  */
 @DisplayName("Data REST - API 테스트")
+@Transactional // 테스트에서 동작하는 트랜잭셔널의 기본 동작은 Rollback 을 위함
 @AutoConfigureMockMvc
-@SpringBootTest
+@SpringBootTest // 통합 테스트
 public class DataRestTest {
     private final MockMvc mvc;
 
@@ -34,17 +36,15 @@ public class DataRestTest {
         this.mvc = mvc;
     }
 
-    @DisplayName("[api] 게시글 리스트 조회")
+    @DisplayName("[api] 게시글 리스트 조행")
     @Test
     void test() throws Exception {
         // Given
 
-        // When
+        // When & Then
         // mockMvc에게 컨트롤러에 대한 정보를 입력
     	mvc.perform(get("/api/articles"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
-        // Then
-
     }
 }
