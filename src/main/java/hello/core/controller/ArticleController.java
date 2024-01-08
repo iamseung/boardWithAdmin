ad264ff7c4e9a463d350fbf764ec83bbc5f811d9
iamseung <seesungseok@naver.com>
2024-01-08 22:19:20 +0900

    페이지 네이션 구현 및 테스트 코드 작성, 현재 인덱스가 가운데에 오게 설계했고 그에 맞게 계산하는 서비스 생성 뷰에 데이터 연동까지 진행

--- src/main/java/hello/core/controller/ArticleController.java
+++ src/main/java/hello/core/controller/ArticleController.java
@@ -4,7 +4,9 @@ import hello.core.domain.type.SearchType;
 import hello.core.dto.response.ArticleResponse;
 import hello.core.dto.response.ArticleWithCommentsResponse;
 import hello.core.service.ArticleService;
+import hello.core.service.PaginationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 /*
     /articles
@@ -27,6 +31,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class ArticleController {
 
     private final ArticleService articleService;
+    private final PaginationService paginationService;
 
     @GetMapping
     public String articles(
@@ -36,7 +41,13 @@ public class ArticleController {
             ModelMap map
     ) {
         // 모든 Entity 의 정보를 모두 다루고 있기 때문에 한번 더 가공한 Dto를 사용하여 반환한다.
-        map.addAttribute("articles", articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from));
+        Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from);
+
+        List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
+
+        map.addAttribute("articles", articles);
+        map.addAttribute("paginationBarNumbers", barNumbers);
+
         return "articles/index";
     }
 
