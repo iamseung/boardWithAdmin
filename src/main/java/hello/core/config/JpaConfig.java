cee5bf144b6580508bf958027e6605b5be2f7a70
iamseung <seesungseok@naver.com>
2024-01-16 20:24:36 +0900

    주석 추가 - @EnableJpaAuditing 설명 추가

--- src/main/java/hello/core/config/JpaConfig.java
+++ src/main/java/hello/core/config/JpaConfig.java
@@ -7,6 +7,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.util.Optional;
 
+/*
+엔티티 객체가 생성이 되거나 변경이 되었을 때 @EnableJpaAuditing 어노테이션을 활용하여 자동으로 값을 등록할 수 있습니다
+ */
 // JpaAuditing 기능 활성화
 @EnableJpaAuditing
 @Configuration
